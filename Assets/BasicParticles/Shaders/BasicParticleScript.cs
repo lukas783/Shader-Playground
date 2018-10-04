57851fc7bcd60cf7d8499032566691fc5bef2435
Lucas Carpenter <lucas.carpenter@mines.sdsmt.edu>
2018-10-03 21:12:02 -0600

    Added a basic particle system, this consists of a single vertex, fragment, and compute shader. An array of particle structures is created and initialized with several characteristics, such as lifetime and velocity. This array is sent to the compute shader where the buffer is updated periodically, the vertex and fragment shader take the buffer from the compute shader and render each pixel procedurally on screen. Currently no geometry shader is implemented, but with tweaking the numbers a few neat effects can be made.

---
+++ Assets/BasicParticles/Shaders/BasicParticleScript.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicParticleScript : MonoBehaviour {
+
+    // a particle structure
+    private struct Particle {
+        public Vector3 position;
+        public Vector3 velocity;
+        public float currTime;
+        public float lifeTime;
+    }
+
+    public int particleCount = 3000000;
+
+    public Material material;
+    public ComputeShader computeShader;
+
+    ComputeBuffer particleBuffer;
+    private int kernelID;
+
+    // set up the array of particles
+	void Start () {
+		if(particleCount <= 0) {
+            particleCount = 1;
+        }
+
+        Particle[] particleArray = new Particle[particleCount];
+        for(int i = 0; i < particleCount; i++) {
+            particleArray[i].position.x = 0;
+            particleArray[i].position.y = 10;
+            particleArray[i].position.z = 0;
+
+            particleArray[i].velocity.x = Random.value * 0.1f - Random.value * 0.1f;
+            particleArray[i].velocity.y = Random.value * 0.1f - Random.value * 0.1f;
+            particleArray[i].velocity.z = Random.value * 0.1f - Random.value * 0.1f;
+
+            particleArray[i].currTime = 0.0f;
+            particleArray[i].lifeTime = Random.value * 5.0f;
+        }
+
+        particleBuffer = new ComputeBuffer(particleCount, 32);
+        particleBuffer.SetData(particleArray);
+        kernelID = computeShader.FindKernel("SpawnParticles");
+        computeShader.SetBuffer(kernelID, "particleBuffer", particleBuffer);
+        material.SetBuffer("particleBuffer", particleBuffer);
+    }
+
+    // what to do when the program exits
+    void OnDestroy() {
+        if (particleBuffer != null)
+            particleBuffer.Release();
+    }
+
+
+    // Update is run every frame, we dispatch our compute shader after setting any constantly changing variables
+    void Update () {
+        computeShader.SetFloat("deltaTime", Time.deltaTime);
+        computeShader.SetFloat("gameTime", Time.time);
+        computeShader.Dispatch(kernelID, Mathf.CeilToInt((float)particleCount/256.0f),1,1);
+		
+	}
+
+    //render as points 
+    void OnRenderObject() {
+        material.SetPass(0);
+        Graphics.DrawProcedural(MeshTopology.Points, 1, particleCount);
+    }
+
+}
---
+++ Assets/BasicParticles/Shaders/BasicParticleShader.shader
@@ -0,0 +1,59 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Custom/BasicParticleShader" {
+	Properties {
+		_Color("Color", Color) = (1,1,1,1)
+	}
+	SubShader {
+
+		Pass {
+
+			CGPROGRAM
+			
+			// The OpenGL Target we wanna hit
+			#pragma target 5.0
+
+			// declare some vertex and fragment shaders ( TODO: ADD IN #pragma geometry geom)
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+
+			// have access to our particle struct from inside the shader
+			struct Particle {
+				float3 position;
+				float3 velocity;
+				float currTime;
+				float lifeTime;
+			};
+		
+			// default vertex/fragment stuff 
+			struct PS_IN {
+				float4 position : SV_POSITION;
+				float4 color : COLOR;
+			};
+	
+			// access our array of particles from inside the shader 
+			StructuredBuffer<Particle> particleBuffer;
+
+			uniform float4 _Color;
+
+			// the fragment shadser, this passes the color through and sets the position of our particle
+			PS_IN vert(uint vertex_id : SV_VertexID, uint instance_id : SV_InstanceID) {
+				PS_IN o = (PS_IN)0;
+
+				o.color = _Color;
+				o.position = UnityObjectToClipPos(float4(particleBuffer[instance_id].position, 1.0f));
+				return o;
+			}
+
+			// the fragment shader, this currently just spits out the material color 
+			float4 frag(PS_IN input) : COLOR
+			{
+				return input.color;
+			}
+
+			ENDCG
+		}
+	}
+}
